0ba4e215e31821acf270b5eb4d936f3aab35ceaa
Frefreak <frefreak.zxy@gmail.com>
2023-04-21 20:46:57 +0800

    test ProcessTextActivity

--- app/src/main/java/nz/carso/thedict/ProcessTextActivity.kt
+++ app/src/main/java/nz/carso/thedict/ProcessTextActivity.kt
@@ -4,12 +4,16 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.runBlocking
 
 class ProcessTextActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val selectedText = intent.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT)
         Toast.makeText(applicationContext, "ProcessTextActivity: $selectedText", Toast.LENGTH_SHORT).show()
+        runBlocking{
+            Utils.addCardWithWord(this@ProcessTextActivity, selectedText.toString())
+        }
         finish()
     }
 }
